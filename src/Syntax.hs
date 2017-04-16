359d89305a9bf50e343d0094214434f2a47baf98
Leonardo Rodrigues <lmrodrigues@inf.ufrgs.br>
2017-04-16 00:21:11 -0300

    Preliminar Syntax Added

---
+++ src/Syntax.hs
@@ -0,0 +1,9 @@
+-- | Implements Syntax and Types terms to L0 Language
+
+module Syntax where
+
+data Terms =
+    TRUE | FALSE
+  | ZERO | SUCC Terms
+  | IF (Terms, Terms, Terms)
+  | ISZERO Terms | PRED Terms
